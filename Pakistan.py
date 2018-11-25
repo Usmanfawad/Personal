9240c717d032101d334c3293ff5c4f35d919a530
choudharyusman <45047731+choudharyusman@users.noreply.github.com>
2018-11-26 02:01:02 +0500

    Add files via upload

---
+++ Pakistan.py
@@ -0,0 +1,18 @@
+from turtle import *
+def curvemove():
+    print(u"\u25A0"*5)
+    print(" DIL")
+    for i in range(200):
+        right(1)
+        forward(1)
+color('red','green')        
+begin_fill()
+left(150)
+forward(111.65)
+curvemove()
+left(100)
+curvemove()
+forward(90)
+print("Pakistan")
+end_fill()
+done()
